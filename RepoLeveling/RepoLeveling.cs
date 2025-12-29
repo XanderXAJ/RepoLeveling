ff0ad3780d847e21655cb3bc2ff876f31081840e
Alex Palmer <XanderXAJ@users.noreply.github.com>
2025-12-29 20:42:26 +0000

    Bump version for disambiguation

--- RepoLeveling/RepoLeveling.cs
+++ RepoLeveling/RepoLeveling.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 namespace RepoLeveling;
 
-[BepInPlugin("Einhornyordle.RepoLeveling", "RepoLeveling", "0.2.0")]
+[BepInPlugin("Einhornyordle.RepoLeveling", "RepoLeveling", "0.2.1")]
 public class RepoLeveling : BaseUnityPlugin
 {
     public static RepoLeveling Instance { get; set; } = null!;
