35b2b4afde23c5faf340058aa1995ebb604970ed
Alex Palmer <XanderXAJ@users.noreply.github.com>
2025-12-29 20:39:54 +0000

    Add logs on applying skills

--- RepoLeveling/PunManangerPatch.cs
+++ RepoLeveling/PunManangerPatch.cs
@@ -9,6 +9,7 @@ internal static class PunManagerPatch
     private static void ReceiveSyncData_Postfix(bool finalChunk)
     {
         if (!finalChunk) return;
+        RepoLeveling.Logger.LogInfo("ReceiveSyncData_Postfix: Conditions met for applying skills.");
         RepoLeveling.Logger.LogDebug("Client sync complete.");
         SaveDataManager.ApplySkills();
     }
--- RepoLeveling/StatsManagerPatch.cs
+++ RepoLeveling/StatsManagerPatch.cs
@@ -10,7 +10,9 @@ internal static class StatsManagerPatch
     [HarmonyPostfix, HarmonyPatch(nameof(StatsManager.PlayerAdd))]
     private static void PlayerAdd_Postfix(string _steamID)
     {
+        RepoLeveling.Logger.LogInfo($"PlayerAdd_Postfix called with _steamID: {_steamID}, Current PlayerSteamID: {PlayerController.instance.playerSteamID}, Avatar SteamID: {PlayerAvatar.instance.steamID}, IsMasterClientOrSingleplayer: {SemiFunc.IsMasterClientOrSingleplayer()}, RunIsLevel: {SemiFunc.RunIsLevel()}");
         if (!SemiFunc.IsMasterClientOrSingleplayer() || !SemiFunc.RunIsLevel() || _steamID != PlayerController.instance.playerSteamID) return;
+        RepoLeveling.Logger.LogInfo("PlayerAdd_Postfix: Conditions met for applying skills.");
         RepoLeveling.Logger.LogDebug("Player data has been added.");
         SaveDataManager.ApplySkills();
     }
